2b14e08d9d075d39d96869ab7b1c1326cd87c120
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-25 10:31:14 +0100

    add paragraph all pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,6 +12,11 @@ export default function Contact() {
 			</header>
 			<main>
 				<h1>Contact Page</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor tempore, aspernatur rem, sint blanditiis, odio
+					nemo inventore laboriosam totam laborum adipisci tempora repudiandae cupiditate dignissimos cum atque illum
+					excepturi beatae.
+				</p>
 			</main>
 			<footer>
 				<p>Copyright 2024</p>
